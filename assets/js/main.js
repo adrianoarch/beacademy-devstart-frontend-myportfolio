9fcb0719e44439d2ea8d416137d104c76fd6520d
Adriano de Oliveira <adriano.98@gmail.com>
2022-05-16 16:42:57 -0300

    Criação dos elementos da section Depoimentos e estilizando com a biblioteca Swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,15 @@ linkWork.forEach(l => l.addEventListener('click', () => {
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestemonial = new Swiper(".testemonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
