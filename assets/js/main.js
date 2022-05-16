0320cf07e60b33c5eca8b95cb016d1db4691d63c
Adriano de Oliveira <adriano.98@gmail.com>
2022-05-16 12:11:47 -0300

    Adicionando link para o script main.js no HTML e criando função para adicionar class scroll-header para criar box-shadow quando rolar 50

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // Quando a rolagem for maior que 50 de altura da janela de visualização, adicione a classe scroll-header à tag de cabeçalho
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
